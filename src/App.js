454c7798fc4f7ce0471e0eedd4038dfe1e7fb58d
Stuart van Beek <stuart@mediamonks.com>
2019-04-25 12:07:22 -0400

    remove unused code

--- src/App.js
+++ src/App.js
@@ -9,67 +9,7 @@ const processFile = async (file) => {
     let stdout = "";
 
     await ffmpeg(file);
-    // let result = await ffmpegjs({
-    //     arguments: [
-    //         '-y',
-    //         '-i',
-    //         file.name,
-    //         "-c:v",
-    //         "libx264",
-    //         "-preset",
-    //         "slower",
-    //         "-crf",
-    //         "21",
-    //         "-c:a",
-    //         "aac",
-    //         "-b:a",
-    //         "96k",
-    //         "-pix_fmt",
-    //         "yuv420p",
-    //         "-profile:v",
-    //         "high",
-    //         "-level",
-    //         "4.1",
-    //         "-movflags",
-    //         "faststart",
-    //         "-vf",
-    //         "scale=-1:404",
-    //         'output.mp4'],
-    //     MEMFS: [{name: file.name, data: data}],
-    //     // stdin: Comlink.proxyValue(() => { }),
-    //     // stdout: Comlink.proxyValue(() => { console.log('out') }),
-    //     // stderr: Comlink.proxyValue(() => { console.log('err') }),
-    //     onfilesready: Comlink.proxyValue((e) => {
-    //         let data = e.MEMFS[0].data;
-    //         const output = URL.createObjectURL(new Blob([data]))
-    //         console.log('ready', data)
-    //     }),
-    //     print: Comlink.proxyValue(function (data) {
-    //         console.log(data);
-    //         stdout += data + "\n";
-    //     }),
-    //     printErr: Comlink.proxyValue(function (data) {
-    //         console.log('error', data);
-    //         stderr += data + "\n";
-    //     }),
-    //     postRun: Comlink.proxyValue(function (result) {
-    //         console.log('DONE', result);
-    //     }),
-    //     postRunCallback: Comlink.proxyValue(function (result) {
-    //         console.log('DONE', result);
-    //     }),
-    //     onExit: Comlink.proxyValue(function (code) {
-    //         console.log("Process exited with code " + code);
-    //         console.log(stdout);
-    //     }),
-    //     exit: Comlink.proxyValue(function (code) {
-    //         console.log("Process exited with code " + code);
-    //         console.log(stdout);
-    //     }),
-    // });
-    //
-    // console.log(result);
-}
+};
 
 function App() {
     const fileRef = React.createRef();
